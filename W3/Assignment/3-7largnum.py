2f4323c42ff94caefa12d3002c601a7107a212eb
Vijay <ssvijayg@gmail.com>
2020-04-09 16:53:18 +0530

    W3 Assignment

--- W3/Assignment/3-7largnum.py
+++ W3/Assignment/3-7largnum.py
@@ -1,5 +1,13 @@
 import sys
 
+def isgreater(dig, maxdig) :
+    ab = str(dig) + str(maxdig)
+    ba = str(maxdig) + str(dig)
+    #x = int(ab)
+    #y = int(ba)
+    return ab >= ba
+
+
 def sal(l, p) :
     x = all(x < 10 for x in p)
     if x :
@@ -7,23 +15,17 @@ def sal(l, p) :
         print(*p, sep="")
         return
     r = []
-    while True :
-        if len(p) == 1:
-            r.append(p[0])
-            print(*r, sep="")
-            return
-        maxx,minn = p.index(max(p)), p.index(min(p))
-        temp = p[maxx]
-        temp = temp // 10
-        if p[minn] < 10 and (p[minn] > temp % 10 and p[minn] >= p[maxx] % 10) :
-            r.append(p[minn])
-            del p[minn]
-        else :
-            r.append(p[maxx])
-            del p[maxx]
-        #x = p[i] if all(p[i] 
+    while len(p) is not 0 :
+        maxdig = -1
+        for digit in p :
+            if isgreater(digit, maxdig) :
+                maxdig = digit
+                i = p.index(digit)
+        r.append(maxdig)
+        del p[i]
+    print(*r, sep="")
+    return
 
-        
 if __name__ == '__main__':
     n = int(sys.stdin.readline())
     input = sys.stdin.readline()
