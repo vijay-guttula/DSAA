59043ee4cf2f8a5bd8209a8b1579bc8d7d54d405
vijay <vjguttula@gmail.com>
2020-05-09 10:22:40 +0530

    W5

--- W4/5lottery.py
+++ W4/5lottery.py
@@ -1,16 +1,20 @@
 import sys
 
+# points are sorted
+# beg and end points are sorted
+# not done yet
 
-def fast(p, b, e):
-    count = 0
-    i = 0
-    while i < len(b):
-        if p < b[i] or p > e[i]:
-            return count
-        if p >= b[i] and p <= e[i]:
+
+def quick():
+    c = []
+
+    while j < len(p):
+        if p[j] >= b[i] and p[j] <= e[i]:
             count += 1
             i += 1
-    return count
+        elif p[j] <= b[i] and p[j] >= e[i]:
+            c.append(count)
+            j += 1
 
 
 def lot_naive(s, p):
@@ -33,17 +37,12 @@ if __name__ == '__main__':
     for i in range(n[0]):
         input = sys.stdin.readline()
         n1 = list(map(int, input.split()))
-        # s.append(n1)
-        b.append(n1[0])
-        e.append(n1[1])
-    b.sort()
-    # print(b)
-    e.sort()
-    # print(e)
-    # print(s[0][1])
+        s.append((n1[0], 'left'))
+        s.append((n1[1], 'right'))
+    # b.sort()
+    # .sort()
+
     input = sys.stdin.readline()
     n1 = list(map(int, input.split()))
-    # print(lot_naive(s, n1))
     for i in n1:
-        # print(i)
-        print(fast(i, b, e), end=" ")
+        s.append((i, 'point'))
