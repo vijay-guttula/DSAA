acc63e388b72d9d81584159314e342855d745428
vijay <vjguttula@gmail.com>
2020-05-16 17:43:20 +0530

    2_W1

--- 2_Data_Structures/W1/2.py
+++ 2_Data_Structures/W1/2.py
@@ -1,22 +1,29 @@
 import sys
 
+'''
+Observation - the numbers not in the list are the leaf nodes.
+but searching from only leaves too takes O(n ** 2)
+
+
 
 def height(n, tree):
-    max = None
+    count = None
+    height = 0
     for i in range(n):
-        count = 0
-        if tree[i] == -1:
-            continue
-        else:
+        if i not in tree:
             j = tree[i]
+            count = 0
             while j != -1:
                 count += 1
                 j = tree[j]
-            if max == None:
-                max = count
-            elif max < count:
-                max = count
-    return max + 1
+            height = max(height, count)
+    return height + 1
+
+'''
+
+
+def height():
+    for i in range(n):
 
 
 if __name__ == '__main__':
@@ -26,4 +33,3 @@ if __name__ == '__main__':
     tree = list(map(int, input.split()))
 
     print(height(n, tree))
-
